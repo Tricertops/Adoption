8645ed4dc247d3cf4899345c07d6568a8afe7c58
Martin Kiss <martin.kiss@me.com>
2015-10-17 10:45:03 +0200

    Setup UI in XIB and UI manipulation methods

--- Widget/TodayViewController.swift
+++ Widget/TodayViewController.swift
@@ -11,5 +11,49 @@ import NotificationCenter
 
 class TodayViewController: NSViewController, NCWidgetProviding {
     
+    let URL: NSURL! = NSURL(string: "https://developer.apple.com/support/app-store/")
+    
+    @IBOutlet var primaryLabel: NSTextField!
+    @IBOutlet var secondaryLabel: NSTextField!
+    
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+    
+    @IBAction func openURL(sender: AnyObject) {
+        NSWorkspace.sharedWorkspace().openURL(self.URL)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        self.showText(primary: "Hello!")
+        
+        self.widgetPerformUpdateWithCompletionHandler { result in }
+    }
+    func widgetPerformUpdateWithCompletionHandler(report: ((NCUpdateResult) -> Void)?) {
+        guard self.URL != nil else {
+            report?(.Failed)
+            return
+        }
+        //TODO: Download and parse website.
+        report?(.NoData)
+        
+    }
+    
+    func showError(detail detail: String = "") {
+        self.showText(primary: "Failed :(", secondary: detail)
+    }
+    
+    func showText(primary primary: String, secondary: String = "") {
+        let invisible = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        let primaryTrimmed = primary.stringByTrimmingCharactersInSet(invisible)
+        let secondaryTrimmed = secondary.stringByTrimmingCharactersInSet(invisible)
+        
+        self.primaryLabel.stringValue = primaryTrimmed
+        self.secondaryLabel.stringValue = secondaryTrimmed
+        
+        self.secondaryLabel.hidden = secondaryTrimmed.isEmpty
+    }
+    
 }
