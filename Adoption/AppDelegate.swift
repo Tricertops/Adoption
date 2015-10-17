6e92dba460697752d57d115dc65e8874d4196f1d
Martin Kiss <martin.kiss@me.com>
2015-10-17 10:41:23 +0200

    Project with App + Widget targets

---
+++ Adoption/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Adoption
+//
+//  Created by Martin Kiss on 14.10.15.
+//  Copyright © 2015 Tricertops. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Widget/TodayViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodayViewController.swift
+//  Widget
+//
+//  Created by Martin Kiss on 14.10.15.
+//  Copyright © 2015 Tricertops. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+    
+        
+}
