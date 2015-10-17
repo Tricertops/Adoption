93df9b93388692ef913a404aab862ea4cce155e3
Martin Kiss <martin.kiss@me.com>
2015-10-17 10:46:58 +0200

    Download and parse the website for two strings

--- Widget/TodayViewController.swift
+++ Widget/TodayViewController.swift
@@ -31,14 +31,87 @@ class TodayViewController: NSViewController, NCWidgetProviding {
         
         self.widgetPerformUpdateWithCompletionHandler { result in }
     }
+    
     func widgetPerformUpdateWithCompletionHandler(report: ((NCUpdateResult) -> Void)?) {
         guard self.URL != nil else {
             report?(.Failed)
             return
         }
-        //TODO: Download and parse website.
-        report?(.NoData)
+        self.showText(primary: "Loading…", secondary: "This may take a while.")
         
+        let task = NSURLSession.sharedSession().dataTaskWithURL(URL) { data, response, error in
+            NSOperationQueue.mainQueue().addOperationWithBlock {
+                guard error == nil else {
+                    self.showError(detail: error!.localizedDescription)
+                    report?(.Failed)
+                    return
+                }
+                guard data != nil else {
+                    self.showError(detail: "There was no error, but also no content.")
+                    report?(.Failed)
+                    return
+                }
+                
+                do {
+                    let document = try NSXMLDocument(data: data!, options: NSXMLDocumentTidyHTML)
+                    let (primary, secondary) = try self.parseDocument(document)
+                    self.showText(primary: primary, secondary: secondary)
+                    
+                    //TODO: Compare previous text with new text.
+                    report?(.NewData)
+                }
+                catch {
+                    self.showError(detail: "Cannot parse the website.")
+                    report?(.Failed)
+                }
+            }
+        }
+        task.resume();
+    }
+    
+    func parseDocument(document: NSXMLDocument) throws -> (primary: String, secondary: String) {
+        var node: NSXMLNode! = document.rootElement()
+        while node != nil {
+            if node.kind == .ElementKind {
+                let element = node as! NSXMLElement
+                
+                if self.isChartDiv(element) {
+                    let headerElement = element.elementsForName("h6").first
+                    let footerElement = element.elementsForName("p").first
+                    if self.isChartHeader(headerElement) && self.isChartFooter(footerElement) {
+                        let header = headerElement?.stringValue ?? ""
+                        let footer = footerElement?.stringValue ?? ""
+                        return (header, footer)
+                    }
+                }
+            }
+            node = node?.nextNode
+        }
+        
+        throw NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo: [:])
+    }
+    
+    func isChartDiv(element: NSXMLElement?) -> Bool {
+        return self.isElement(element, ofName: "div", ofClass: "chart")
+    }
+    
+    func isChartHeader(element: NSXMLElement?) -> Bool {
+        return self.isElement(element, ofName: "h6")
+    }
+    
+    func isChartFooter(element: NSXMLElement?) -> Bool {
+        return self.isElement(element, ofName: "p", ofClass: "footnote")
+    }
+    
+    func isElement(element: NSXMLElement?, ofName: String, ofClass: String = "") -> Bool {
+        guard let element = element else { return false }
+        guard element.name == ofName else { return false }
+        if ofClass.isEmpty { return true }
+        guard let classNode = element.attributeForName("class") else { return false }
+        guard let classValue = classNode.stringValue else { return false }
+        guard let classValues: NSArray = classValue.componentsSeparatedByString(" ") else { return false }
+        guard classValues.containsObject(ofClass) else { return false }
+        return true
     }
     
     func showError(detail detail: String = "") {
